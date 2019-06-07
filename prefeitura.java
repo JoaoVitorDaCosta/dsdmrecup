c8647137bf3f235bce49c62a076fe453b627e586
JoaoVitorDaCosta <41134275+JoaoVitorDaCosta@users.noreply.github.com>
2019-06-07 15:14:16 -0300

    Add files via upload

---
+++ prefeitura.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+int edificio;
+int casa;
+int casa_de_condominio;
+int apartamento;
+int comercial;
+float area;
+String=iptu;
+
+public class casa{
+
+	public satic void main(String[]args){
+		Scanner trab = new scanner(System.in);
+		System.out.println("informe a area do edificio: ");
+		area = trab.next();
+		System.out.println("informe qual o edificio: ");
+		edificio = input.next();
+
+		if (edificio = casa) {
+
+			iptu=area*7.00;
+
+			System.out.println(" O valor de IPTU a ser pago eh: "+iptu);
+	 }
+	
+
+}
+
+}
+
+public class apartamento{
+	public static void main(String[]args){
+		Scanner trab = new scanner(System.in);
+		System.out.println("informe a area do edificio: ");
+		area = trab.next();
+		System.out.println("informe qual o edificio: ");
+		edificio = input.next();
+		
+		if (edificio = apartamento) {
+
+			iptu=area*8.50;
+
+			System.out.println(" O valor de IPTU a ser pago eh: "+iptu);
+	 }
+	 
+	}
+}
+public class comercial{
+	public static void main(String[] args) {
+		Scanner trab = new scanner(System.in);
+		System.out.println("informe a area do edificio: ");
+		area = trab.next();
+		System.out.println("informe qual o edificio: ");
+		edificio = input.next();
+
+		if (edificio =comercial) {
+
+			iptu=area*10.70;
+
+			System.out.println(" O valor de IPTU a ser pago eh: "+iptu);
+	 }
+		
+	}
+
+}
+public class casa_de_condominio{
+	public static void main(String[] args) {
+		Scanner trab = new scanner(System.in);
+		System.out.println("informe a area do edificio: ");
+		area = trab.next();
+		System.out.println("informe qual o edificio: ");
+		edificio = input.next();
+
+		if (edificio=casa_de_condominio) {
+
+			iptu=area*9.00;
+
+			System.out.println(" O valor de IPTU a ser pago eh: "+iptu)
+			
+		}
+		
+	}
+}
